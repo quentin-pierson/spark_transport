361c7401ed418d94429daa56b368080e9e0a39a9
quentin-pierson <quentinprs62@gmail.com>
2022-06-02 13:07:47 +0200

    add target to gitignore

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "spark_transport"
+
+version := "0.1"
+
+scalaVersion := "2.12.15"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.2.1"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.2.1"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "3.2.1"
---
+++ src/main/scala/test.scala
@@ -0,0 +1,28 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object test extends App{
+
+  val sparkSession = SparkSession
+    .builder()
+    .master("local[*]")
+    .appName("streaming")
+    .getOrCreate()
+
+  sparkSession.sparkContext.setLogLevel("ERROR")
+
+  sparkSession.conf.set("spark.sql.shuffle.partitions","5")
+
+  val retaildf = sparkSession
+    .read
+    .format("csv")
+    .option("inferSchema","true")
+    .option("header","true")
+    .option("delimiter",";")
+    .load("src/data/test.csv")
+
+    retaildf.printSchema()
+    retaildf.show(false)
+  
+}
+
